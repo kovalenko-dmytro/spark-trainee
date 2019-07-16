540fcbb7f43fb191a38fa7191b45bdd87d5aada9
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2019-07-16 15:09:02 +0300

    (spark-trainee): init project

---
+++ src/main/java/com/kovalenko/sparktrainee/SparkTraineeApplication.java
@@ -0,0 +1,13 @@
+package com.kovalenko.sparktrainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkTraineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkTraineeApplication.class, args);
+	}
+
+}
